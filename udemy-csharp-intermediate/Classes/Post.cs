c099dea32e244a54fd6a6c44b6bb4a4b4b7813ff
Christian Vasquez <christianvasquezmelo@gmail.com>
2017-12-11 00:37:08 -0400

    Add Post class to Classes folder

---
+++ udemy-csharp-intermediate/Classes/Post.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace udemy_csharp_intermediate.Classes
+{
+    public class Post
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int Votes { get; private set; }
+        public DateTime DateCreated { get; }
+
+        public Post(string title, string description)
+        {
+            Title = title;
+            Description = description;
+            DateCreated = DateTime.Now;
+        }
+
+        public void UpVote()
+        {
+            Votes++;
+        }
+
+        public void DownVote()
+        {
+            Votes--;
+        }
+    }
+}
