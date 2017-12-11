71c6def65506e025ab196c3be88da936bda3075b
Christian Vasquez <christianvasquezmelo@gmail.com>
2017-12-11 09:34:43 -0400

    Add SqlConnection to Polymorphism folder

---
+++ udemy-csharp-intermediate/Polymorphism/SqlConnection.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace udemy_csharp_intermediate.Polymorphism
+{
+    public class SqlConnection : DbConnection
+    {
+        public SqlConnection(string connectionString) : base(connectionString)
+        {
+        }
+
+        public override void Open()
+        {
+            Console.WriteLine("SqlConnection was opened.");
+        }
+
+        public override void Close()
+        {
+            Console.WriteLine("SqlConnection was closed.");
+        }
+    }
+}
