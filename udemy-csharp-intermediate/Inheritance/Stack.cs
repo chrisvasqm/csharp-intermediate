6e548ce5bbac104717c37ec68c689fcb5717e2bc
Christian Vasquez <christianvasquezmelo@gmail.com>
2017-12-11 01:09:46 -0400

    Add Stack class to Inheritance folder

---
+++ udemy-csharp-intermediate/Inheritance/Stack.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace udemy_csharp_intermediate.Inheritance
+{
+    public class Stack
+    {
+        private readonly List<object> _items = new List<object>();
+
+        public void Push(object obj)
+        {
+            _items.Add(obj);
+        }
+
+        public object Pop()
+        {
+            var last = _items.Last();
+            _items.Remove(last);
+            return last;
+        }
+
+        public void Clear()
+        {
+            _items.Clear();
+        }
+    }
+}
