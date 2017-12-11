969ccdeaea851985ac2714fb3e2f04af571f2bcd
Christian Vasquez <christianvasquezmelo@gmail.com>
2017-12-11 15:31:41 -0400

    Add concrete Activity classes

---
+++ udemy-csharp-intermediate/Interface/FirstActivity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace udemy_csharp_intermediate.Interface
+{
+    public class FirstActivity : IActivity
+    {
+        public void Execute()
+        {
+            Console.WriteLine("Executing first activity");
+        }
+    }
+}
---
+++ udemy-csharp-intermediate/Interface/SecondActivity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace udemy_csharp_intermediate.Interface
+{
+    public class SecondActivity : IActivity
+    {
+        public void Execute()
+        {
+            Console.WriteLine("Executing second activity");
+        }
+    }
+}
