10d3a95c715c6bdb938ba0d6b8230a11ed609952
Christian Vasquez <christianvasquezmelo@gmail.com>
2017-12-11 15:32:24 -0400

    Add WorkflowEngine class

---
+++ udemy-csharp-intermediate/Interface/WorkflowEngine.cs
@@ -0,0 +1,13 @@
+﻿namespace udemy_csharp_intermediate.Interface
+{
+    public class WorkflowEngine
+    {
+        public void Run(Workflow workflow)
+        {
+            foreach (var activity in workflow.Activities)
+            {
+                activity.Execute();
+            }
+        }
+    }
+}
