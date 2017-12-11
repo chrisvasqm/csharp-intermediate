096b570838d379f4c71d2b88e3554682f1aafbc6
Christian Vasquez <christianvasquezmelo@gmail.com>
2017-12-11 15:31:58 -0400

    Add Workflow class

---
+++ udemy-csharp-intermediate/Interface/Workflow.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace udemy_csharp_intermediate.Interface
+{
+    public class Workflow
+    {
+        public List<IActivity> Activities { get; }
+
+        public Workflow(List<IActivity> activities)
+        {
+            Activities = activities;
+        }
+    }
+}
