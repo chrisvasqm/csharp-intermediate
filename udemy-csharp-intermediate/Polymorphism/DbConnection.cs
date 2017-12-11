01f087712f1e323328945f6be6a9a797c0b9f6b1
Christian Vasquez <christianvasquezmelo@gmail.com>
2017-12-11 09:30:53 -0400

    Add DbConnection to Polymorphism folder

---
+++ udemy-csharp-intermediate/Polymorphism/DbConnection.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace udemy_csharp_intermediate.Polymorphism
+{
+    public abstract class DbConnection
+    {
+        private readonly string _connectionString;
+        private readonly TimeSpan _timeout;
+
+        protected DbConnection(string connectionString)
+        {
+            if (String.IsNullOrWhiteSpace(connectionString))
+                throw new ArgumentException("The connection string cannot be null, whitespace or empty");
+
+            _connectionString = connectionString;
+        }
+
+        public abstract void Open();
+
+        public abstract void Close();
+    }
+}
