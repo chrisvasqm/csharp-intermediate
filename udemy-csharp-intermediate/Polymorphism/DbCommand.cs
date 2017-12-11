d9e23ab20cc431b5f0eca14ac07f5ffd0dbee3f8
Christian Vasquez <christianvasquezmelo@gmail.com>
2017-12-11 11:08:48 -0400

    Add DbCommand to Polymorphism folder

---
+++ udemy-csharp-intermediate/Polymorphism/DbCommand.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace udemy_csharp_intermediate.Polymorphism
+{
+    public class DbCommand
+    {
+        private readonly DbConnection _dbConnection;
+        private readonly string _instruction;
+
+        public DbCommand(DbConnection dbConnection, string instruction)
+        {
+            if (dbConnection == null)
+                throw new ArgumentException("DbConnection cannot be null.");
+            if (String.IsNullOrWhiteSpace(instruction))
+                throw new ArgumentException("The instruction cannot be null, whitespace or empty");
+            
+            _dbConnection = dbConnection;
+            _instruction = instruction;
+        }
+
+        public void Execute()
+        {
+            _dbConnection.Open();
+            Console.WriteLine("Instruction: " + _instruction);
+            _dbConnection.Close();
+        }
+    }
+}
