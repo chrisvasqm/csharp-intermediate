10f2cd30512e283e7ced42bdaf5eff0b2523a9af
Christian Vasquez <christianvasquezmelo@gmail.com>
2017-12-13 22:21:18 -0400

    Add null check for each activity

--- udemy-csharp-intermediate/Interface/WorkflowEngine.cs
+++ udemy-csharp-intermediate/Interface/WorkflowEngine.cs
@@ -1,4 +1,6 @@
-﻿namespace udemy_csharp_intermediate.Interface
+﻿using System;
+
+namespace udemy_csharp_intermediate.Interface
 {
     public class WorkflowEngine
     {
@@ -6,6 +8,9 @@
         {
             foreach (var activity in workflow.Activities)
             {
+                if (activity == null)
+                    throw new ArgumentException("Activities cannot be null.");
+                
                 activity.Execute();
             }
         }
